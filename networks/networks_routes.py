a92f4f488206e60e4da27b3889bbc276895f4955
Andrew Furman <aifurman@gmail.com>
2025-04-27 14:29:29 +0000

    changed to be just provider focus

--- networks/networks_routes.py
+++
@@ -1,8 +0,0 @@
-
-from flask import Blueprint, render_template
-
-networks_bp = Blueprint('networks', __name__, template_folder='templates')
-
-@networks_bp.route('/networks')
-def networks():
-    return render_template('networks.html')
